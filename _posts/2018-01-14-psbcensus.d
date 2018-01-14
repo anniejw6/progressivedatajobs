3b799410858d8ea96231506cf2b6d790bdd692de
lenatomai <ltom@analystinstitute.org>
2018-01-14 14:55:40 -0600

    Create 2018-01-14-psbcensus.d

---
+++ _posts/2018-01-14-psbcensus.d
@@ -0,0 +1,62 @@
+---
+layout: single
+title:  "PSB Research: Data Scientist"
+tags: 
+    - analyst
+    - data
+    - datamanagement
+---
+
+Moderator's note: this firm does not wholly fit the definition we follow for job postings on this site typically, but given the critical important of census data, we are opting to post this and similar positions as they arise.
+
+PSB Research seeks a qualified Data Scientist to join our team in Washington, DC, for an exciting opportunity to work on data analytics
+and research as part of the 2020 Census Integrated Communications Campaign. The 2020 Census Integrated Communications Campaign team
+supports all outreach and marketing for the 2020 Decennial Census. PSB is the research agency on the Integrated Communications Contract Team,
+working with our partners to develop, refine and test the marketing and communications strategy used to ensure as many Americans as
+possible are reached during the critical, every-ten-year Census.
+
+The Data Scientist will be based in PSB’s offices in Washington, DC, but may work at times from the client site in Prince George’s County,
+MD. S/he will be involved in manipulating large data sets, building analytical tools and models, and creating and presenting challenging
+data science materials to audiences of all levels and backgrounds, both internally and externally. S/he will be a senior presence on the
+team designing and building models and analyses that drive smart insights and at times will work across all partners on the Integrated
+Communications Contract team, including advertising and creative agencies, media buying entities, and niche communications firms.
+
+PSB is an Affirmative Action and Equal Opportunity Employer.
+
+Responsibilities:
+
+Most critically, we are looking for unmatched team players who are excited about being part of a once-in-a-career effort to mold the
+delivery of the largest social marketing campaign in the country’s history. Background and specific areas of expertise are less important
+than drive, initiative, and willingness to take on items outside of your day-to-day role. That said, we expect the Data Scientist to bring
+a set of core skills to the table (outlined below).
+
+At a high level, the Data Scientist will be responsible for a variety of tasks, including but not limited to:
+* Turning complex client problems into robust data solutions and fully owning the client’s data science needs from conception to delivery of final product
+* Working with integrated teams of advertising creatives, researchers, social scientists and consultants to create a strong data-driven point of view while addressing all partners’ needs
+* Conceptualize and build predictive models from large data sets to address client needs; translate findings into smart, actionable, and timely insights
+* Using and evaluating clustering techniques to identify actionable segments within large datasets and provide flexible profiling for external validation of the results
+* Authoring clear, concise and technically sophisticated reports and presenting findings to clients (internal and external) of all levels and backgrounds; proactively identifying opportunities to build from these findings to next steps or new applications
+* Working across the Integrated Communications Contract team on contract management efforts, including proposal writing, pricing and scoping projects
+* Managing and serving as a mentor to more junior members of the team, both on the data science team and outside, and upward manage to the appropriate senior members of the team
+
+Qualifications
+
+Basic Qualifications:
+
+The successful candidate will possess:
+* A minimum of 5+ years’ professional experience working in applied data science
+* BA required/MA preferred in a related field (statistics, math, economics, physics, political science, computer science)
+* Proven success working with large and complex datasets in a federal environment (SQL and SAS experience preferred)
+* Proficiency with statistical programs including R, and/or Python
+* Proven success in consulting environments presenting findings using presentation and/or data visualization software.
+* Authorization to work in the U.S. and to complete a federal government background clearance process
+
+Ideal Qualifications:
+
+The ideal candidate will also possess:
+* Familiarity developing data science solutions as part of a larger data science team
+* Knowledge of both supervised and unsupervised machine learning techniques
+* Deep experience presenting solutions in a federal/government context; ability to quickly put client challenges into a federal context when presenting
+* Business development experience with client stakeholders, including pricing, scoping, and presentation
+* Exceptional management skill and an interest in mentoring and guiding more junior members of the team
+* Membership in professional organization such as AAPOR, WAPOR, Insights Association, or ASA
