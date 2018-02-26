420723860552e7616d2a6d45b1284b366f45376f
lenatomai <ltom@analystinstitute.org>
2018-02-26 16:18:02 -0600

    Create 2018-02-26-targetsmartpolling.m

---
+++ _posts/2018-02-26-targetsmartpolling.m
@@ -0,0 +1,67 @@
+---
+layout: single
+title:  "TargetSmart: Research Analysts"
+tags: 
+    - analyst
+    - analytics
+    - campaigns
+    - consultants
+    - data
+    - vendor
+    - vendors
+---
+
+Research Analyst
+Research & Analytics Job Posting – Multiple Analyst Positions
+TargetSmart is immediately seeking analysts for our Research & Analytics department to support our growing public opinion research business. Multiple full-time, permanent positions are available with options for either telework nationwide or to work in our downtown Washington D.C. office space.
+
+About TargetSmart Research & Analytics
+TargetSmart is the Democratic party and progressive movement’s leading provider of voter and consumer data. TargetSmart Research & Analytics is a boutique polling and data-analytics practice nested inside the larger data company. We conduct political and issue-advocacy public opinion research in the form of telephone polls, online polls, and various qualitative methods, in addition to building predictive models for both TargetSmart’s national voter file and custom client needs. Sitting at the juncture of all things political data, we’re uniquely situated to help our clients win by making sound, data-driven resource allocation decisions that leverage all of the strategically actionable intelligence and targeting tools at their disposal.
+
+Responsibilities
+The core responsibilities for all analyst positions will revolve around supporting the life-cycle of our public opinion research polls:
+* Candidate, district, and issue research
+* Data work to investigate past voting patterns and make future turnout projections
+* Questionnaire/survey drafting, editing, and proofing
+* Participation in client-facing interactions including candidate interviews, poll revisions, presentation of poll findings, and on-going strategy calls
+* Survey administration and fielding quality control
+* Survey report auditing
+* Development of analytical deliverables and strategic recommendation from polls including written, narrative reports, PowerPoint presentations, and other data visualization techniques
+* Auditing draft paid campaign communications (direct mail concepts, TV/radio scripts, digital ads, etc.) to ensure they align with strategic findings from polling
+* Support business development through proposal writing and subcontractor research
+
+Qualifications
+The Research & Analytics department is hiring multiple analysts at this time, and seeking those with a range of professional experience for a range of positions that will have varying levels of responsibility and accompanying autonomy and independence. All potential applicants should have the following:
+
+* Strong commitment to the progressive movement and helping Democrats win
+* A borderline compulsive attention to detail
+* Strong verbal, writing, and communication skills
+* Basic data and statistics competency
+* Proficiency in MS Word, Excel, and PowerPoint
+
+Entry-to-mid level positions will be considered for applicants who fit ANY of these criteria:
+* Are recent college graduates looking for an entry-level position in the survey research world
+* Have worked on 1-2 political campaigns (those with a cycle or two of field and/or finance experience are strongly encouraged to apply!)
+* Have worked in an entry-level position for a reputable organization in the progressive space
+* Have fewer than three years’ worth of experience working in the public opinion research space including internships or other entry level positions
+
+Higher level positions will be considered for applicants who fit ANY of these criteria:
+* Have at least three years worth of working experience in the public opinion research space, including internships or other entry level positions
+* Have substantial (minimum three cycles) political campaign experience at the manager, director, or deputy levels and been consumers of campaign polling data
+* Have worked for a reputable organization in the progressive space (non-profit, direct action, advocacy, or consulting) for at least five years and been consumers of issue or campaign polling data
+* Have significant academic experience in the public opinion research space
+
+Though definitely not a pre-requisite to apply, please let us know if you have any of the following skills:
+* Background in data science
+* Background in survey methodology
+* Proficiency using advanced statistical software like R, SPSS, STATA, SAAS, or Alteryx
+* Non-English language proficiency or fluency and specifically ability to translate from English to Spanish and vice a versa
+* Experience working in a call center or personally conducting surveys
+
+Compensation and Benefits
+Compensation for each position will be commensurate with experience. TargetSmart offers a competitive benefits package including health care, dental, vision, life, generous paid time off, home office stipend, Metro stipend, and the ability to telework regardless of whether you live in the DC region or not. It is our intention for these to be permanent (NOT election cycle) positions, and that opportunities for growth inside the company will be available to whomever ultimately accepts them.
+
+How to apply
+Please send resume, cover letter (including how you learned about the positions), references, and salary requirements to jobs@targetsmart.com.
+
+[See more and apply here](https://targetsmart.com/careers/)
